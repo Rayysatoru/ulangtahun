afbf6ea27d49861a2cbce83b34073879c9e44726
Rayysatoru <112258591+Rayysatoru@users.noreply.github.com>
2022-09-19 00:36:34 +0700

    Update effect.js

--- effect.js
+++ effect.js
@@ -159,7 +159,7 @@ $('document').ready(function(){
 		$('#b77').animate({top:240, left: vw+250},500);
 		$('.balloons').css('opacity','0.9');
 		$('.balloons h2').fadeIn(3000);
-		$(this).fadeOut('slow').delay(300000).promise().done(function(){
+		$(this).fadeOut('slow').delay(3000).promise().done(function(){
 			$('#story').fadeIn('slow');
 		});
 	});
@@ -177,7 +177,7 @@ $('document').ready(function(){
 			i=i+1;
 			$("p:nth-child("+i+")").fadeIn('slow').delay(1000);
 			if(i==50){
-				$("p:nth-child(49)").fadeOut('slow').promise().done(function () {
+				$("p:nth-child(30000)").fadeOut('slow').promise().done(function () {
 					$('.cake').fadeIn('fast');
 				});
 				
